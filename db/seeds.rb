251c7a82b8f7a1adba9cd30c9be2cb244d4bf2e4
cc <code723@gmail.com>
2015-09-16 16:21:41 +0800

    udpate

--- db/seeds.rb
+++ db/seeds.rb
@@ -24,15 +24,7 @@ Role.create! name: :student
 Role.create! name: :teacher
 
 puts 'DEFAULT USERS'
-user = User.create! :name=>'oxcafe', :email=>'oxcafe@126.com', :password=>'11111111'
+user = User.create! name: ENV['ADMIN_NAME'].dup, email: ENV['ADMIN_EMAIL'].dup, password: ENV['ADMIN_PASSWORD'].dup
 user.add_role :admin
 user.add_role :teacher
 user.save
-user = User.create! :name=>'basicme', :email=>'basicme@sina.cn', :password=>'11111111'
-user.add_role :user
-user.save
-User.create! :name=>'test', :email=>'test@sina.cn', :password=>'11111111'
-user = User.create! :name=>'test1', :email=>'test1@sina.cn', :password=>'11111111'
-user.add_role :guest
-user.save
-
